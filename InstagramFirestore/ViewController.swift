c188e8885af38e5af8978d6f58b5f49a3d29a83c
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-03-26 00:52:16 -0300

    feat: init project

---
+++ InstagramFirestore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirestore
+//
+//  Created by WAGNER GOULART on 26/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
