550ba931de05b867c03a787f3f98edefd06045ef
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-03-26 01:04:23 -0300

    feat: maintabcontroller created

---
+++ InstagramFirestore/Controller/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  InstagramFirestore
+//
+//  Created by WAGNER GOULART on 26/03/22.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .red
+  }
+}
