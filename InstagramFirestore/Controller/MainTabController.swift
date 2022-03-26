ec7f4c643878825429c4fe403dd5c9925fc20415
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-03-26 04:50:28 -0300

    feat: icons inserted a tabbar menu

--- InstagramFirestore/Controller/MainTabController.swift
+++ InstagramFirestore/Controller/MainTabController.swift
@@ -19,16 +19,21 @@ class MainTabController: UITabBarController {
 
   func configureViewController() {
     view.backgroundColor = .white
-    let feed = FeedController()
-    let search = SearchController()
-    let imageSelector = ImageSelectorController()
-    let notifications = NotificationController()
-    let profile = ProfileController()
+    let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController())
+    let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
+    let imageSelector = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorController())
+    let notifications = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationController())
+    let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController())
 
     viewControllers = [feed, search, imageSelector, notifications, profile]
   }
   func templateNavigationController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController) -> UINavigationController {
-    
+    let nav = UINavigationController(rootViewController: rootViewController)
+    nav.tabBarItem.image = unselectedImage
+    nav.tabBarItem.selectedImage = selectedImage
+    nav.navigationBar.tintColor = .black
+
+    return nav
   }
 
 }
